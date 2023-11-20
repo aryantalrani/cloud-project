effeb3507e475bcdc0a643e3c0b534bf506ab7dd
anshtalrani <114902471+aryantalrani@users.noreply.github.com>
2023-11-20 23:48:58 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,19 @@
+Python:
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! This is a Flask app on AWS Elastic Beanstalk.'
+
+if __name__ == '__main__':
+    app.run()
+
+
+Requirements: 
+
+
+Flask==1.1.2
 
